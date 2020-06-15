9a3c1299552e020b794eb44cdb5dc11dcf5b44ce
Kienan Stewart <kienan@koumbit.org>
2020-06-15 14:35:05 -0400

    Fix typo in request type check

--- src/usr/lib/alternc/generate_certbot.php
+++ src/usr/lib/alternc/generate_certbot.php
@@ -54,7 +54,7 @@ $domains = in_array('domains', array_keys($options)) ? $options['domains'] : (
     in_array('d', array_keys($options)) ? $options['d'] : NULL);
 if ($domains !== NULL) {
     $domains = explode(',', $domains);
-    $REQUEST_CERTS = 'sepcific';
+    $REQUEST_CERTS = 'specific';
 }
 $force_request = (in_array('f', array_keys($options)) || in_array('force', array_keys($options))) ? True : False;
 
